f2163385e8b651ded6444528ba09bb3d883694b3
surendra kamble <133906520+surendrakamble07@users.noreply.github.com>
2025-10-08 20:40:37 +0530

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         Date currentDate = new Date();
-        System.out.println("Hello, Docker! Current date: " + currentDate);
+        System.out.println("Hello, my name is surendra kamble and i am studying today docker! Current date: " + currentDate);
     }
 }
